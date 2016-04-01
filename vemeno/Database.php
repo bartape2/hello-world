4db35e061d2ec8f976fa8a1ab545e415c8575ca0
bartape2 <bartape2@gmail.com>
2016-04-01 15:06:14 +0200

    Added files via upload

---
+++ vemeno/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Vemeno;
+
+class Database
+{
+  
+  protected $db = null;
+  
+  
+  public function __cosntruct()
+  {
+    $this->db = true;    
+  }
+  
+  public function foo()
+  {
+    return "bar";
+  }
+}
---
+++ vemeno/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Vemeno;
+
+class Product
+{
+  
+  protected $id = null;
+  protected $name = "";
+  
+  
+  public function __cosntruct()
+  {
+    $this->id = 42;
+    $this->name = "Teplo US";    
+  }
+  
+  public function foo()
+  {
+    return $this->id.": ".$this->name;
+  }
+}
